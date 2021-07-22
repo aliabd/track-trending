e3d5a6650a832eb47d430a4c36f804431055e1de
aliabd <ali.si3luwa@gmail.com>
2021-07-22 11:21:02 -0700

    send mail

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, render_template
+from flask_mail import Mail, Message
 import requests
 
 app = Flask(__name__)
+
+app.config.update(dict(
+    DEBUG=True,
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USE_SSL=False,
+    MAIL_USERNAME='githubtrendingtracker@gmail.com',
+    MAIL_PASSWORD='github123',
+))
+
+mail = Mail(app)
 
 URLS = {'daily': 'https://trendings.herokuapp.com/repo?lang=python&since=daily',
          'weekly': 'https://trendings.herokuapp.com/repo?lang=python&since=weekly',
@@ -18,5 +31,10 @@ def trending_github(url):
 @app.route("/")
 def index():
     trending = [interval for interval in URLS if trending_github(URLS[interval])]
+    if trending:
+        msg = Message("Gradio is trending on Github!",
+                      sender="ali.si3luwa@gmail.com",
+                      recipients=["ali.si3luwa@gmail.com"])
+        mail.send(msg)
     return render_template("index.html", trending=trending)
 
